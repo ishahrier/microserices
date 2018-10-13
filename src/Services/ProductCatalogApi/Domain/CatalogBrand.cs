491871266f87f9c6c8d53686ea17f20a3c3f25d9
ronnie.nsu@Outlook.com <ronnie.nsu@Outlook.com>
2018-10-13 03:31:53 -0500

    none

--- src/Services/ProductCatalogApi/Domain/CatalogBrand.cs
+++ src/Services/ProductCatalogApi/Domain/CatalogBrand.cs
@@ -1,6 +1,6 @@
 namespace ProductCatalogApi.Domain {
     public class CatalogBrand {
         public int Id { get; set;  }
-        public string Brand { get; set; }
+        public string Brand { get; set; } 
     }
 }

e5ca84103a5efc6654b7034ec4058580ce8df9de
shahrier <ronnie.nsu@outlook.com>
2018-10-15 02:10:38 -0500

    ...

---
+++ src/Services/ProductCatalogApi/ViewModels/PaginatedItemsViewModel.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace ProductCatalogApi.ViewModels {
+    public class PaginatedItemsViewModel<T> where T : class {
+        public PaginatedItemsViewModel (int pageIndex, int pageSize, int count, IEnumerable<T> data) {
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            Count = count;
+            Data = data;
+        }
+        public int PageSize { get; private set; }
+        public int PageIndex { get; private set; }
+        public int Count { get; private set; }
+        public IEnumerable<T> Data { get; set; }
+    }
+}

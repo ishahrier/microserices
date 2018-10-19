cb41e8d4805f9325385017d0df4fc9e374f47d7d
shahrier <ronnie.nsu@outlook.com>
2018-10-18 23:13:38 -0500

    ..

---
+++ src/Web/WebMvc/ViewModels/CatalogIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿ using System.Collections.Generic;
+ using System.Linq;
+ using System.Threading.Tasks;
+ using System;
+ using Microsoft.AspNetCore.Mvc.Rendering;
+ using WebMvc.Models;
+ using WebMvc.ViewModels;
+
+ namespace WebMvc.ViewModels {
+     public class CatalogIndexViewModel {
+         public IEnumerable<CatalogItem> CatalogItems { get; set; }
+         public IEnumerable<SelectListItem> Brands { get; set; }
+         public IEnumerable<SelectListItem> Types { get; set; }
+         public int? BrandFilterApplied { get; set; }
+         public int? TypesFilterApplied { get; set; }
+         public PaginationInfo PaginationInfo { get; set; }
+     }
+ }
